09779708e858a1fae0aa865980950546de25bd39
Khayot <61094811+xkingt@users.noreply.github.com>
2022-04-09 00:03:15 +0500

    Add files via upload

---
+++ scripts/project/scriptsInEvents.js
@@ -0,0 +1,16 @@
+
+
+
+const scriptsInEvents = {
+
+		async Game_over_events_Event1_Act3(runtime, localVars)
+		{
+			console.log(runtime.globalVars.Score);
+			TelegramGameProxy.shareScore(runtime.globalVars.Score);
+			
+		}
+
+};
+
+self.C3.ScriptsInEvents = scriptsInEvents;
+
